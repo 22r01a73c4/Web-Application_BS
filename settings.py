b4387a7324678dee61fe926d8096ed5940d47e0f
Manaswi Thallapally <161413701+22r01a73c4@users.noreply.github.com>
2025-06-26 22:42:44 +0530

    settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,13 @@
+For development:
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'noreply@bluestock.in'
+DEFAULT_FROM_EMAIL = 'man123@gmail.com'
+
+For production
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yourmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@example.com'
+EMAIL_HOST_PASSWORD = 'your-password'
